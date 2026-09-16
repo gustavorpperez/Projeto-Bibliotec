db4642b6bf103cea7c4d59548b5685d7044dfe62
GUSTAVO RODRIGUES PADOVAN PEREZ <54869342898@info.local>
2026-09-16 10:55:18 -0300

    :tada: reserva js iniciada

---
+++ home/script.js
@@ -0,0 +1,50 @@
+let nome = "Diogo Felipe";
+let idade = 25;
+
+// CONCATENAÇÃO
+console.log("Nome: " + nome + ", idade: " + idade);
+
+// INTERPOLAÇÃO
+console.log(`Nome: ${nome}, idade: ${idade}`)
+
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+let soma = "5" + 3 // 53 string // =3 string
+let sub = "5" -3 // não vai rodar / -3 string
+
+console.log(soma)
+console.log(sub)
+
+console.log(typeof soma)
+console.log(typeof sub)
+
+let numero = "ab" // string
+
+// let convertido = Number(numero)
+
+// console.log(typeof convertido)
+
+// OPERADORES RELACIONAIS
+
+
+// let opcao = 2
+
+// switch(opcao) {
+//     case 1 : dhfaojfaoifja
+//             break;
+// }
+
+// ESTRUTURA DE REPETIÇÃO
+
+let frutas = ["Maçã", "Banana", "Acerola", "Uva"]
+
+console.log(Frutas);
+
+frutas.forEach(function(nome) {
+    console.log("Nome: " + fruta);
+})
+
+
+
